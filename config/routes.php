1c650eeee4e472e8b945f33954e1a420f4f4b4f6
Alex SaM <alsam3637@gmail.com>
2026-04-09 14:34:48 -0400

    Created GuestMiddleware. Passed the GuestMiddleware to the routes.

--- config/routes.php
+++ config/routes.php
@@ -5,16 +5,17 @@ use App\Controllers\HomeController;
 use App\Controllers\LoginController;
 use App\Controllers\RegisterController;
 use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
 use Core\Router\Route;
 
 return [
     Route::get('/home' , [HomeController::class , 'index']),
     Route::get('/books', [BookController::class , 'index']),
     Route::get('/admin/books/add', [BookController::class , 'add'], [AuthMiddleware::class]),
-    Route::post('/admin/books/add', [BookController::class , 'store'], [AuthMiddleware::class]),
-    Route::get('/register', [RegisterController::class , 'index']),
+    Route::post('/admin/books/add', [BookController::class , 'store']),
+    Route::get('/register', [RegisterController::class , 'index'], [GuestMiddleware::class]),
     Route::post('/register', [RegisterController::class , 'register']),
-    Route::get('/login', [LoginController::class , 'index']),
+    Route::get('/login', [LoginController::class , 'index'], [GuestMiddleware::class]),
     Route::post('/login', [LoginController::class , 'login']),
     Route::post('/logout', [LoginController::class , 'logout']),
 
