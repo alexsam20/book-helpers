aab63756393ed4a91087438c282e07c1f8846043
Alex SaM <alsam3637@gmail.com>
2026-04-09 11:24:20 -0400

    Passed the Middleware to the routes.

--- config/routes.php
+++ config/routes.php
@@ -4,13 +4,14 @@ use App\Controllers\BookController;
 use App\Controllers\HomeController;
 use App\Controllers\LoginController;
 use App\Controllers\RegisterController;
+use App\Middleware\AuthMiddleware;
 use Core\Router\Route;
 
 return [
     Route::get('/home' , [HomeController::class , 'index']),
     Route::get('/books', [BookController::class , 'index']),
-    Route::get('/admin/books/add', [BookController::class , 'add']),
-    Route::post('/admin/books/add', [BookController::class , 'store']),
+    Route::get('/admin/books/add', [BookController::class , 'add'], [AuthMiddleware::class]),
+    Route::post('/admin/books/add', [BookController::class , 'store'], [AuthMiddleware::class]),
     Route::get('/register', [RegisterController::class , 'index']),
     Route::post('/register', [RegisterController::class , 'register']),
     Route::get('/login', [LoginController::class , 'index']),
