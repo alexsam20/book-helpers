ee9fd38e9e03b64fa642734e291305b1bf23d9b8
Alex SaM <alsam3637@gmail.com>
2026-05-17 11:26:45 -0400

    Added Middleware to the all blosk Admin size. Fixed Logout

--- views/pages/books.php
+++ views/pages/books.php
@@ -34,20 +34,11 @@
                             </a>
                         </div>
                     </li>
-                    <!-- Third element breadcrumb -->
-                    <!--<li aria-current="page">
-                        <div class="flex items-center">
-                            <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"></path>
-                            </svg>
-                            <span class="ms-1 text-sm font-semibold text-gray-500 md:ms-2 dark:text-gray-400">JavaScript - Полный Курс JavaScript Для Начинающих</span>
-                        </div>
-                    </li>-->
                 </ol>
             </nav>
             <!-- Page Content -->
             <div class="text-gray-800 dark:text-gray-400 border border-gray-200 dark:border-blue-900 dark:bg-gray-950/10 rounded-t-2xl mt-3 mb-4">
-                <div class="grid grid-cols-1 p-4 dark:bg-gray-950/50 rounded-t-2xl">
+                <div class="grid grid-cols-1 p-4 bg-gray-100 dark:bg-gray-950/50 rounded-t-2xl">
                     <!-- Cards Block -->
                     <div class="container">
                         <h1 class="text-xl font-bold text-cyan-600 m-3 p-1">Books & Video tutorial</h1>
@@ -80,41 +71,19 @@
                                         </div>
                                         <!-- Footer -->
                                         <div class="p-4 mt-auto text-right">
-                                        <span>
                                             <a href="#" class="inline-flex items-center text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-base text-sm px-3 py-1.5 text-center leading-5">
                                                 Read more
                                                 <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                                     <polyline points="13 17 18 12 13 7"></polyline><polyline points="6 17 11 12 6 7"></polyline>
                                                 </svg>
                                             </a>
-                                        </span>
-                                            <?php if ($auth->check()): ?>
-                                            <div class="flex justify-between mt-3">
-                                                <!--Button Remove-->
-                                                <a href="#" class="inline-flex items-center text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-base text-sm px-3 py-1.5 text-center leading-5">
-                                                    <svg class="w-5 h-5 mb-0.5 mr-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
-                                                    </svg>
-                                                    Remove
-                                                </a>
-                                                <!--Button Edit-->
-                                                <a href="#" class="inline-flex items-center text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-base text-sm px-3 py-1.5 text-center leading-5">
-                                                    <svg class="w-5 h-5 mb-0.5 mr-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z"/>
-                                                    </svg>
-                                                    Edit
-                                                </a>
-                                            </div>
-                                            <?php endif; ?>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-
-
+                    <!-- !! End Cards Block -->
                 </div>
             </div>
         </div>
